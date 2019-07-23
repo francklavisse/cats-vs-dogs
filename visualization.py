9a5c72360e5d95a0e1e5916a6b55d2b2276a3354
Franck Lavisse <franck@alka.app>
2019-07-23 16:58:33 +0200

    Display random pictures of differents animals

--- visualization.py
+++ visualization.py
@@ -4,14 +4,17 @@ import random
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-_, _, cat_images = next(os.walk('Dataset/PetImages/Cat'))
+def display_random_pictures(prefix='Cat'):
+    _, _, pet_images = next(os.walk('Dataset/PetImages/' + prefix))
 
-fig, ax = plt.subplots(3,3, figsize=(20,10))
+    fig, ax = plt.subplots(3,3, figsize=(20,10))
 
-for idx, img in enumerate(random.sample(cat_images, 9)):
-    img_read = plt.imread('Dataset/PetImages/Cat/' + img)
-    ax[int(idx / 3), idx % 3].imshow(img_read)
-    ax[int(idx / 3), idx % 3].axis('off')
-    ax[int(idx / 3), idx % 3].set_title('Cat/' + img)
+    for idx, img in enumerate(random.sample(pet_images, 9)):
+        img_read = plt.imread('Dataset/PetImages/' + prefix + '/' + img)
+        ax[int(idx / 3), idx % 3].imshow(img_read)
+        ax[int(idx / 3), idx % 3].axis('off')
+        ax[int(idx / 3), idx % 3].set_title(prefix + '/' + img)
 
-plt.show()
+    plt.show()
+
+display_random_pictures('Dog')
