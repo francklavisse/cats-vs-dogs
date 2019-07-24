e6856e0fe60f90829fb34c332555a737fad08db8
Franck Lavisse <franck@alka.app>
2019-07-24 14:24:28 +0200

    Try to predict where the model is efficient

--- vgg16.py
+++ vgg16.py
@@ -2,6 +2,7 @@ from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.layers import Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
+from visualization import plot_on_grid
 
 INPUT_SIZE = 128
 
@@ -36,3 +37,33 @@ test_set = testing_data_generator.flow_from_directory('Dataset/PetImages/Test/',
                                                       class_mode='binary')
 
 model.fit_generator(training_set, steps_per_epoch=STEPS_PER_EPOCH, epochs=EPOCHS, verbose=1)
+
+# model analysis
+test_set = testing_data_generator.flow_from_directory('Dataset/PetImages/Test/',
+                                                      target_size=(INPUT_SIZE, INPUT_SIZE),
+                                                      batch_size=1,
+                                                      class_mode='binary')
+
+strongly_wrong_idx = []
+strongly_right_idx = []
+weakly_wrong_idx = []
+
+for i in range(test_set.__len__()):
+    img = test_set.__getitem__(i)[0]
+    pred_prob = model.predict(img)[0][0]
+    pred_label = int(pred_prob > 0.5)
+    actual_label = int(test_set.__getitem__(i)[1][0])
+    
+    if pred_label != actual_label and (pred_prob > 0.8 or pred_prob < 0.2):
+        strongly_wrong_idx.append(i)
+    elif pred_label != actual_label and (pred_prob > 0.4 and pred_prob < 0.6):
+        weakly_wrong_idx.append(i)
+    elif pred_label == actual_label and (pred_prob > 0.8 or pred_prob < 0.2):
+        strongly_right_idx.append(i)
+        
+    if (len(strongly_right_idx) >= 9 and len(strongly_wrong_idx) >= 9 and len(weakly_wrong_idx) >= 9):
+        break    
+
+plot_on_grid(test_set, strongly_right_idx)
+plot_on_grid(test_set, strongly_wrong_idx)
+plot_on_grid(test_set, weakly_wrong_idx)
--- visualization.py
+++ visualization.py
@@ -16,5 +16,14 @@ def display_random_pictures(prefix='Cat'):
         ax[int(idx / 3), idx % 3].set_title(prefix + '/' + img)
 
     plt.show()
+
+def plot_on_grid(test_set, idx_to_plot, img_size=128):
+    fig, ax = plt.subplots(3, 3, figsize=(20,10))
+    for i, idx in enumerate(random.sample(idx_to_plot, 9)):
+        img = test_set.__getitem__(idx)[0].reshape(img_size, img_size, 3)
+        ax[int(i / 3), i % 3].imshow(img)
+        ax[int(i / 3), i % 3].axis('off')    
+        
+    plt.show()
     
 display_random_pictures('Dog')
